7e275e75a055c08a57211c15624ce36df07c4b05
JaelFaridDeras <faridderas@gmail.com>
2018-05-19 22:11:48 -0500

    Primera parte
    Creacion de login, registro de usuario y panel de contro.

---
+++ laravel/resources/views/users/partials/form.blade.php
@@ -0,0 +1,29 @@
+
+<div class="form-group">
+	{{ Form::label('name', 'Nombre') }}
+	{{ Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) }}
+</div>
+<hr>
+<div class="form-group">
+	{{ Form::label('email', 'Email') }}
+	{{ Form::text('email', null, ['class' => 'form-control', 'id' => 'email']) }}
+</div>
+<hr>
+
+<h3>Lista de roles</h3>
+<div class="form-group">
+	<ul class="list-unstyled">
+		@foreach($roles as $role)
+	    <li>
+	        <label>
+	        {{ Form::checkbox('roles[]', $role->id, null) }}
+	        {{ $role->name }}
+	        <em>({{ $role->description ?: 'N/A' }})</em>
+	        </label>
+	    </li>
+	    @endforeach
+    </ul>
+</div>
+<div class="form-group">
+	{{ Form::submit('Guardar', ['class' => 'btn btn-sm btn-primary']) }}
+</div>
